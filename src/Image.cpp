f602c8296b6ae048b1b2607386858b0277a58d69
zlogdanbr <54685402+zlogdanbr@users.noreply.github.com>
2022-05-28 18:09:21 -0300

    Add files via upload

--- src/Image.cpp
+++ src/Image.cpp
@@ -68,6 +68,27 @@ void FindAndDFacesAndEyes(const std::string& image_path)
     showImage(img, "Final");
 }
 
+void FindAndDFacesAndEyes(Mat& img)
+{
+    int scale = 1;
+
+    std::vector<Rect> faces = detectFacesInImage(img);
+
+    for (auto& roi : faces)
+    {
+        drawCirclesAtImgFromRoi(img, roi);
+        Mat faceROI = convertRectoImg(roi, img);
+        std::vector<Rect> eyes = detectEyesInImage(faceROI);
+        for (auto& roieye : eyes)
+        {
+            drawCirclesAtImgFromRoi(faceROI, roieye);
+        }
+    }
+
+
+    showImage(img, "Final");
+}
+
 /**
 * ImageAdjustmentsTesting
 * it tests adjusments in a photo specified by the path image_path
@@ -125,30 +146,30 @@ void ImageAdjustmentsTesting(const std::string& image_path)
 * Finds edges in a poto specified by the path image_path using the laplacian
 * algorithm.
 */
-void FindEdgesOnPhoto(const std::string& image_path)
+Mat FindEdgesOnPhoto(const std::string& image_path)
 {
-    Mat img;
+    Mat img; 
 
     if (loadImage(image_path, img) == false)
     {
         std::cout << "Could not read the image: " << image_path << std::endl;
-        return;
+        return img;
     }
 
-    Mat imglp = laplacian(img);
+    //showImage(img, "Original" );
+    //showImage(imglp, "Laplacian");
 
-    showImage(img, "Original" );
-    showImage(imglp, "Laplacian");
-
-    auto Imfinal = convertograyScale(img) - imglp;
+    auto Imfinal = convertograyScale(img) - laplacian(img);
     showImage(Imfinal, "Final");
+    return Imfinal;
 }
 
 int main()
 {
     //std::string image_path = samples::findFile("eu.jpg");
-    std::string image_path = "C:\\code\\image\\Image\\Image\\x64\\Debug\\f.jpg";
-    FindAndDFacesAndEyes(image_path);
+    std::string image_path = "C:\\code\\image\\Image\\Image\\x64\\Debug\\eu.jpg";
+    Mat img = FindEdgesOnPhoto(image_path);
+    FindAndDFacesAndEyes(img);
     waitKey(0); // Wait for a keystroke in the window
     destroyAllWindows();
     return 0;
