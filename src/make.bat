5aaf77594336cd1094d2c0bfe81ed02cafed5d5e
Daniel <daniel.vasconcelosgomes@gmail.com>
2023-10-18 10:15:11 -0300

    restrutured

--- src/make.bat
+++ src/make.bat
@@ -1,10 +1,13 @@
 echo off
 clear
 IF "%~1"=="" GOTO _BUILD
-IF "%~1"=="--clean" python build.py --clean
+IF "%~1"=="--clean" python build.py --clean ELSE GOTO _SINGLE
 GOTO _END
 :_BUILD
 python build.py --clean
 python build.py --project
+GOTO _END
+:_SINGLE
+python build.py %~1
 :_END
 
